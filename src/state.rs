f9c361218d357046526d468c877f462e0031b03c
OttoBrown <henrynlyman@gmail.com>
2022-05-06 10:52:02 -0600

    update to eframe 0.18.0

--- src/state.rs
+++ src/state.rs
@@ -1,22 +1,18 @@
-use eframe::{egui, epi};
+use eframe::egui;
+use eframe::{CreationContext, Frame};
 use egui::Context;
-use epi::Frame;
 
 use crate::ui;
 
 pub struct State {}
 impl State {
-    pub fn new() -> Self {
+    pub fn new(_cc: &CreationContext) -> Self {
         State {}
     }
 }
 
-impl epi::App for State {
-    fn update(&mut self, ctx: &Context, frame: &Frame) {
+impl eframe::App for State {
+    fn update(&mut self, ctx: &Context, frame: &mut Frame) {
         ui::render(self, ctx, frame);
-    }
-
-    fn name(&self) -> &str {
-        "Egui App"
     }
 }
